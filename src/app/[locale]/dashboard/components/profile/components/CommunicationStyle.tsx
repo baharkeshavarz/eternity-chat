d485063e75d7dba32476f229745daa3605472e66
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2025-07-25 21:01:36 +0330

    chore: refactor the commuication style in edit info

--- src/app/[locale]/dashboard/components/profile/components/CommunicationStyle.tsx
+++ src/app/[locale]/dashboard/components/profile/components/CommunicationStyle.tsx
@@ -1,58 +1,93 @@
-import RollbackSwitch from '@/components/RollbackSwitch/RollbackSwitch';
-import { Divider, Grid, Typography } from '@mui/material';
+import { CustomTextField } from '@/components/Fields';
+import { DASHBOARD_FORM_LABELS } from '@/constants/general';
+import { CommunicationPayload } from '@/services/onboarding/types';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Box, Divider, Grid, Typography } from '@mui/material';
 import { useLocale, useTranslations } from 'next-intl';
-import { useCallback } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import SaveButton from '../../common/SaveButton';
+import { sharedTextFieldProps } from '../../common/SharedStyles';
 
 const CommunicationStyle = () => {
   const t = useTranslations();
   const locale = useLocale();
 
-  const items = [
-    {
-      key: 'email',
-      title: t('pages.settings.notifications.email'),
-      defaultValue: false,
-    },
-    {
-      key: 'inApp',
-      title: t('pages.settings.notifications.inApp'),
-      defaultValue: true,
-    },
-    {
-      key: 'push',
-      title: t('pages.settings.notifications.push'),
-      defaultValue: true,
-    },
-  ];
+  const labels: Record<keyof CommunicationPayload, string> = {
+    description: 'Way of Talking',
+    saying: 'Phrase',
+    lovedVoice: 'Voice',
+    textVoice: 'Text Messages',
+  };
 
-  const onChange = useCallback((key: string, value: boolean) => {
-    console.log(`Changed ${key} to`, value);
-  }, []);
+  const resolveSchema: yup.ObjectSchema<CommunicationPayload> = yup.object({
+    description: yup.string().nullable().required().label(labels.description),
+    saying: yup.string().nullable().required().label(labels.saying),
+    lovedVoice: yup.string().label(labels.lovedVoice),
+    textVoice: yup.string().label(labels.textVoice),
+  });
 
+  const methods = useForm<CommunicationPayload>({
+    resolver: yupResolver(resolveSchema),
+    defaultValues: {
+      saying: 'Keep Smiling!',
+    },
+  });
+  const { control } = methods;
   const typoClass = `latoStyleRegular-${locale}`;
 
   return (
-    <>
-      {items?.map((item) => (
-        <>
-          <Grid container py={2.5} key={item.key}>
-            <Grid size={{ xs: 6 }}>
-              <Typography variant="subtitle1" className={typoClass}>
-                {item.title}
-              </Typography>
-            </Grid>
-            <Grid size={{ xs: 6 }} textAlign="right">
-              <RollbackSwitch
-                size="small"
-                value={item.defaultValue}
-                onChange={() => onChange(item.key, true)}
-              />
-            </Grid>
-          </Grid>
-          <Divider />
-        </>
-      ))}
-    </>
+    <FormProvider {...methods}>
+      <Grid container alignItems="center" py={1}>
+        <Grid size={{ xs: 3 }}>
+          <Typography
+            variant="subtitle1"
+            fontWeight={400}
+            color={DASHBOARD_FORM_LABELS}
+            className={typoClass}
+          >
+            {labels.description}
+          </Typography>
+        </Grid>
+        <Grid size={{ xs: 9 }}>
+          <CustomTextField
+            label=""
+            name="description"
+            variant="standard"
+            className={typoClass}
+            {...sharedTextFieldProps}
+          />
+        </Grid>
+      </Grid>
+      <Divider />
+
+      <Grid container alignItems="center" py={1}>
+        <Grid size={{ xs: 3 }}>
+          <Typography
+            variant="subtitle1"
+            fontWeight={400}
+            color={DASHBOARD_FORM_LABELS}
+            className={typoClass}
+          >
+            {labels.saying}
+          </Typography>
+        </Grid>
+        <Grid size={{ xs: 9 }}>
+          <CustomTextField
+            label=""
+            name="saying"
+            variant="standard"
+            className={typoClass}
+            {...sharedTextFieldProps}
+          />
+        </Grid>
+      </Grid>
+      <Divider />
+
+      <Box mt={4} display="flex" justifyContent="flex-end">
+        <SaveButton />
+      </Box>
+    </FormProvider>
   );
 };
 
